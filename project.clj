8acf1c2da8746abd16f6c0c8dfea91f31cccca6b
Joris Bontje <jorisbontje@gmail.com>
2011-02-19 14:17:45 +0100

    Upgrade to ring 0.3.6, compojure 0.6.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-core "0.3.5"]
-                 [ring/ring-devel "0.3.5"]
-                 [compojure "0.5.3"]]
+                 [ring/ring-core "0.3.6"]
+                 [ring/ring-devel "0.3.6"]
+                 [compojure "0.6.0"]]
   :dev-dependencies [[appengine-magic "0.4.0-SNAPSHOT"]])
